9287233418866658fc52cd9d09b59552ae505391
farchenko90 <fandresroja@gmail.com>
2016-12-15 00:02:47 -0500

    profesores

--- public_html/js/scripts/Controladores/MenuControlador.js
+++ public_html/js/scripts/Controladores/MenuControlador.js
@@ -4,18 +4,21 @@ app.controller('MenuControlador', function ($scope) {
 	$scope.activar = function(val){
 		switch(val){
 			case 1: $scope.active = "active";
+					$scope.title = "Materias";
 					$scope.active1 = "";
 					$scope.active2 = "";
 				break;
 			case 2: $scope.active1 = "active";
+					$scope.title = "Profesores";
 					$scope.active = "";
 					$scope.active2 = "";
 				break;
 			case 3: $scope.active2 = "active";
+					$scope.title = "Estudiantes";
 					$scope.active1 = "";
 					$scope.active = "";
 				break;
-
+				
 		}
 	}
 	
