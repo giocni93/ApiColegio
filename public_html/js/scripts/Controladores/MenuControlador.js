f32fc5882e5fc747c74a97895e0bd3ec8e719edc
Gilmar Ocampo Nieves <giocni@gmail.com>
2016-12-15 15:53:26 -0500

    nuevo api

--- public_html/js/scripts/Controladores/MenuControlador.js
+++ public_html/js/scripts/Controladores/MenuControlador.js
@@ -1,7 +1,15 @@
 app.controller('MenuControlador', function ($scope) {
 
 	$scope.active = "";
+	$scope.banMenu = [true,false,false,false];
 	$scope.activar = function(val){
+		for(var i = 0; i < $scope.banMenu.length; i++){
+			$scope.banMenu[i] = false;
+			if(i == val){
+				$scope.banMenu[i] = true;
+			}
+		}
+		/*
 		switch(val){
 			case 1: $scope.active = "active";
 					$scope.title = "Materias";
@@ -18,8 +26,12 @@ app.controller('MenuControlador', function ($scope) {
 					$scope.active1 = "";
 					$scope.active = "";
 				break;
-				
-		}
+			case 4: $scope.active2 = "active";
+					$scope.title = "Estudiantes";
+					$scope.active1 = "";
+					$scope.active = "";
+				break;
+		}*/
 	}
 	
 });
