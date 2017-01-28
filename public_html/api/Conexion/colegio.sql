a926a9321d6ea23b4d3f76c569f3093cc7b0d99c
Gilmar Ocampo Nieves <giocni@gmail.com>
2017-01-28 16:49:14 -0500

    nuevo sql colegio

--- public_html/api/Conexion/colegio.sql
+++ public_html/api/Conexion/colegio.sql
@@ -1,5 +1,24 @@
-CREATE DATABASE IF NOT EXISTS `colegio` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `colegio`;
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-01-2017 a las 22:48:35
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `colegio`
+--
 
 -- --------------------------------------------------------
 
@@ -32,7 +51,11 @@ CREATE TABLE `menu` (
 
 INSERT INTO `menu` (`id`, `idModulo`, `item`, `ruta`, `icono`, `estado`) VALUES
 (6, 1, 'Profesores', 'menu.html#!/gestion/profesores', 'android', 'ACTIVO'),
-(7, 1, 'Materias', 'menu.html#!/gestion/materias', 'android', 'ACTIVO');
+(7, 1, 'Materias', 'menu.html#!/gestion/materias', 'android', 'ACTIVO'),
+(8, 4, 'Usuarios', 'menu.html#!/seguridad/usuarios', NULL, 'ACTIVO'),
+(10, 2, 'Vigencias', '/configuracion/vigencias', NULL, 'ACTIVO'),
+(11, 1, 'Estudiantes', '/gestion/estudiantes', NULL, 'ACTIVO'),
+(12, 1, 'Observaciones', '/gestion/observaciones', NULL, 'ACTIVO');
 
 -- --------------------------------------------------------
 
@@ -55,7 +78,8 @@ CREATE TABLE `modulo` (
 INSERT INTO `modulo` (`id`, `nombre`, `abierto`, `icon`, `estado`) VALUES
 (1, 'GESTIÓN', 1, 'android', 'ACTIVO'),
 (2, 'CONFIGURACIÓN', 0, NULL, 'ACTIVO'),
-(3, 'REPORTES', 0, NULL, 'ACTIVO');
+(3, 'REPORTES', 0, NULL, 'ACTIVO'),
+(4, 'SEGURIDAD', 0, NULL, 'ACTIVO');
 
 -- --------------------------------------------------------
 
@@ -85,7 +109,7 @@ INSERT INTO `perfil` (`id`, `nombre`) VALUES
 
 CREATE TABLE `permisos` (
   `id` int(10) UNSIGNED NOT NULL,
-  `idPerfil` int(10) UNSIGNED NOT NULL,
+  `idUsuario` int(10) UNSIGNED NOT NULL,
   `idModulo` int(10) UNSIGNED NOT NULL,
   `idMenu` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -94,10 +118,11 @@ CREATE TABLE `permisos` (
 -- Volcado de datos para la tabla `permisos`
 --
 
-INSERT INTO `permisos` (`id`, `idPerfil`, `idModulo`, `idMenu`) VALUES
-(1, 1, 1, 6),
-(2, 1, 2, NULL),
-(3, 1, 3, NULL);
+INSERT INTO `permisos` (`id`, `idUsuario`, `idModulo`, `idMenu`) VALUES
+(12, 1, 4, 8),
+(13, 10, 1, 6),
+(14, 10, 1, 7),
+(15, 10, 2, 10);
 
 -- --------------------------------------------------------
 
@@ -107,11 +132,15 @@ INSERT INTO `permisos` (`id`, `idPerfil`, `idModulo`, `idMenu`) VALUES
 
 CREATE TABLE `profesor` (
   `id` int(10) UNSIGNED NOT NULL,
-  `nombre_apellido` varchar(50) NOT NULL,
-  `cedula` varchar(11) NOT NULL,
-  `celular` varchar(11) NOT NULL,
-  `correo` varchar(50) NOT NULL
+  `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `profesor`
+--
+
+INSERT INTO `profesor` (`id`, `nombre`) VALUES
+(1, 'jasjdajsdjas');
 
 -- --------------------------------------------------------
 
@@ -123,15 +152,25 @@ CREATE TABLE `usuario` (
   `id` int(10) UNSIGNED NOT NULL,
   `idPerfil` int(10) UNSIGNED NOT NULL,
   `identificacion` varchar(50) NOT NULL,
-  `contrasena` varchar(100) NOT NULL
+  `email` varchar(60) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `contrasena` varchar(100) NOT NULL,
+  `estado` enum('ACTIVO','INACTIVO') NOT NULL,
+  `usuarioCreacion` varchar(50) DEFAULT NULL,
+  `fechaCreacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `usuarioModificacion` varchar(50) DEFAULT NULL,
+  `fechaModificacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `idPerfil`, `identificacion`, `contrasena`) VALUES
-(1, 1, '1065', 'fc5424208c1b537f900688bde93966ed7c1380d2');
+INSERT INTO `usuario` (`id`, `idPerfil`, `identificacion`, `email`, `telefono`, `contrasena`, `estado`, `usuarioCreacion`, `fechaCreacion`, `usuarioModificacion`, `fechaModificacion`) VALUES
+(1, 1, '1065', NULL, NULL, 'fc5424208c1b537f900688bde93966ed7c1380d2', 'ACTIVO', NULL, '2016-12-17 22:45:44', NULL, '2016-12-17 22:45:44'),
+(7, 1, 'giocni', NULL, NULL, '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'ACTIVO', '1065', '2016-12-18 03:03:30', '1065', '2016-12-18 03:03:30'),
+(9, 2, 'pepe', 'pepe@gmail.com', '5840351', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'ACTIVO', 'giocni', '2016-12-18 03:31:22', 'giocni', '2016-12-18 03:31:22'),
+(10, 1, 'junior', 'junior@gmail.com', '1241312412', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'ACTIVO', '1065', '2017-01-20 15:46:33', '1065', '2017-01-20 15:46:33');
 
 --
 -- Índices para tablas volcadas
@@ -167,7 +206,7 @@ ALTER TABLE `perfil`
 --
 ALTER TABLE `permisos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `idPerfil` (`idPerfil`),
+  ADD KEY `idPerfil` (`idUsuario`),
   ADD KEY `idModulo` (`idModulo`),
   ADD KEY `idMenu` (`idMenu`);
 
@@ -182,6 +221,7 @@ ALTER TABLE `profesor`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `identificacion` (`identificacion`),
   ADD KEY `idPerfil` (`idPerfil`);
 
 --
@@ -197,32 +237,32 @@ ALTER TABLE `materia`
 -- AUTO_INCREMENT de la tabla `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `modulo`
 --
 ALTER TABLE `modulo`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `perfil`
 --
 ALTER TABLE `perfil`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `permisos`
 --
 ALTER TABLE `permisos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `profesor`
 --
 ALTER TABLE `profesor`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- Restricciones para tablas volcadas
 --
@@ -237,7 +277,7 @@ ALTER TABLE `menu`
 -- Filtros para la tabla `permisos`
 --
 ALTER TABLE `permisos`
-  ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`idPerfil`) REFERENCES `perfil` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `permisos_ibfk_2` FOREIGN KEY (`idModulo`) REFERENCES `modulo` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `permisos_ibfk_3` FOREIGN KEY (`idMenu`) REFERENCES `menu` (`id`) ON UPDATE CASCADE;
 
@@ -246,3 +286,7 @@ ALTER TABLE `permisos`
 --
 ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idPerfil`) REFERENCES `perfil` (`id`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
